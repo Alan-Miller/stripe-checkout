560246141b28afa0496497951e01f1c1203ca784
Alan-Miller <alan.ashman@gmail.com>
2017-10-26 20:43:10 -0600

    gitignored files and started server

---
+++ README.md
@@ -0,0 +1,1 @@
+Example of Stripe Check (simple) in React. Based on [Joe Blank's repo](https://github.com/joeblank/react-stripe).
---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+    , bodyParser = require('body-parser')
+    , cors = require('cors')
+    , stripe = require('stripe')
+    , app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(3001, _ => { console.log(`Listening on ${3001}.`)});
