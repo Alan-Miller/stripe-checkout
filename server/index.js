f7d444e7e9b7a4991d18e1a75bb37f05de083d94
Alan-Miller <alan.ashman@gmail.com>
2018-03-01 23:10:42 -0700

    comment

--- server/index.js
+++ server/index.js
@@ -9,6 +9,9 @@ app.use(bodyParser.json());
 app.use(express.static(`${__dirname}/../build`));
 
 app.post('/api/payment', (req, res, next) => {
+
+    // If needed, convert req.body.amount to pennies
+
     const charge = stripe.charges.create(
         {
             amount: req.body.amount,
